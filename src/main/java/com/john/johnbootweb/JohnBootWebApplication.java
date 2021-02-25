064c72e381e5ec5341d116b73be0e931daf61983
huangjy40 <huangjy40>
2021-02-25 13:40:05 +0800

    init commit

---
+++ src/main/java/com/john/johnbootweb/JohnBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.john.johnbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JohnBootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/john/johnbootweb/JohnBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.johnbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
